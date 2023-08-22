2cf122684ee8b5c6273efc7ba1daba1db954f8be
Glauber Marques <glauberfmarques@gmail.com>
2023-08-22 07:35:02 -0600

    Implementing similar images to modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import HomeRoute from './components/HomeRoute';
+import PhotoFavButton from './components/PhotoFavButton';
 import './App.scss';
 import './styles/PhotoDetailsModal.scss';
 import similarPhotos from './mocks/photos';
@@ -18,6 +19,16 @@ const App = () => {
     setSelectedPhotoData(null);
   };
 
+  // Function to toggle favorite for the main photo
+  const toggleFavorite = (photoId) => {
+    // Add your logic to toggle favorite for the main photo
+  };
+
+  // Determine if the main photo is favorited
+  const isMainPhotoFavorited = () => {
+    // Add your logic to check if the main photo is favorited
+  };
+
   return (
     <div className="App">
       <HomeRoute
@@ -32,22 +43,22 @@ const App = () => {
           <button className="photo-details-modal__close-button" onClick={closeModal}>
             X
           </button>
-          {/* Display modal image */}
-          <div className="photo-details-modal__images">
+          {/* Display modal image */}<div className="photo-details-modal__images">
             <img src={selectedPhotoData.urls.full} className="photo-details-modal__image" alt={`Larger Photo by ${selectedPhotoData.user.username}`} />
+            <div className="photo-details-modal__header">Similar Photos</div>
+            {/* Display similar photos */}
+            <div className="photo-details-modal__similar-photos">
+              {similarPhotos.slice(0, 3).map(photo => (
+                <img
+                  key={photo.id}
+                  src={photo.urls.regular}
+                  className="photo-details-modal__similar-photo"
+                  alt={`Similar Photo by ${photo.user.username}`}
+                />
+              ))}
+            </div>
           </div>
-          {/* Display similar photos */}
-          <div className="photo-details-modal__similar-photos">
-            {similarPhotos.slice(0, 3).map(photo => (
-              <img
-                key={photo.id}
-                src={photo.urls.small}
-                className="photo-details-modal__similar-photo"
-                alt={`Similar Photo by ${photo.user.username}`}
-              />
-            ))}
           </div>
-        </div>
       )}
     </div>
   );
