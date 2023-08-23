f05b90fddcd10d49533fda04a53b7da8da3f282b
Glauber Marques <glauberfmarques@gmail.com>
2023-08-23 06:28:05 -0600

    Updating the custom hook to use redeuce instead of just managing state as was before

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,39 +1,73 @@
 // hooks/useApplicationData.js
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+export const ACTIONS = {
+  TOGGLE_MODAL: 'TOGGLE_MODAL',
+  CLOSE_MODAL: 'CLOSE_MODAL',
+  TOGGLE_FAVORITE: 'TOGGLE_FAVORITE',
+};
+
+const initialState = {
+  isModalOpen: false,
+  selectedPhotoData: null,
+  favoritedPhotos: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.TOGGLE_MODAL:
+      return {
+        ...state,
+        isModalOpen: !state.isModalOpen,
+        selectedPhotoData: action.payload,
+      };
+    case ACTIONS.CLOSE_MODAL:
+      return {
+        ...state,
+        isModalOpen: false,
+        selectedPhotoData: null,
+      };
+    case ACTIONS.TOGGLE_FAVORITE:
+      const photoId = action.payload;
+      const favoritedPhotos = state.favoritedPhotos.includes(photoId)
+        ? state.favoritedPhotos.filter(id => id !== photoId)
+        : [...state.favoritedPhotos, photoId];
+      return {
+        ...state,
+        favoritedPhotos,
+      };
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
 
 const useApplicationData = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedPhotoData, setSelectedPhotoData] = useState(null);
-  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleModal = (photoData) => {
-    setIsModalOpen(!isModalOpen);
-    setSelectedPhotoData(photoData);
+    dispatch({ type: ACTIONS.TOGGLE_MODAL, payload: photoData });
   };
 
   const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedPhotoData(null);
+    dispatch({ type: ACTIONS.CLOSE_MODAL });
   };
 
   const toggleFavorite = (photoId) => {
-    if (favoritedPhotos.includes(photoId)) {
-      setFavoritedPhotos(favoritedPhotos.filter(id => id !== photoId));
-    } else {
-      setFavoritedPhotos([...favoritedPhotos, photoId]);
-    }
+    dispatch({ type: ACTIONS.TOGGLE_FAVORITE, payload: photoId });
   };
 
   const isPhotoFavorited = (photoId) => {
-    return favoritedPhotos.includes(photoId);
+    return state.favoritedPhotos.includes(photoId);
   };
 
   return {
-    isModalOpen,
-    selectedPhotoData,
+    isModalOpen: state.isModalOpen,
+    selectedPhotoData: state.selectedPhotoData,
     toggleModal,
     closeModal,
-    favoritedPhotos,
+    favoritedPhotos: state.favoritedPhotos,
     toggleFavorite,
     isPhotoFavorited,
   };
