657472a29a28abb4785159e4b36ee6a3119b86f2
Glauber Marques <glauberfmarques@gmail.com>
2023-08-28 21:22:23 -0600

    Move the reducer and the actions to a separate file and import it inside the useApplicationData.js to improve code modularity and reusability

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,17 +1,6 @@
 import { useEffect, useReducer } from "react";
+import { reducer, ACTIONS } from "./reducer";
 
-// Define action types for the reducer
-export const ACTIONS = {
-  TOGGLE_MODAL: 'TOGGLE_MODAL',
-  CLOSE_MODAL: 'CLOSE_MODAL',
-  TOGGLE_FAVORITE: 'TOGGLE_FAVORITE',
-  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
-  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
-  FETCH_TOPIC_PHOTOS: 'FETCH_TOPIC_PHOTOS',
-  RESET_TOPIC_PHOTOS: 'RESET_TOPIC_PHOTOS',
-};
-
-// Define the initial state for the reducer
 const initialState = {
   isModalOpen: false,
   selectedPhotoData: null,
@@ -21,50 +10,6 @@ const initialState = {
   currentTopic: null
 };
 
-// Define the reducer function to handle different actions
-function reducer(state, action) {
-  switch (action.type) {
-    case ACTIONS.SET_PHOTO_DATA:
-      return { ...state, photoData: action.payload 
-      };
-    case ACTIONS.SET_TOPIC_DATA:
-      return { ...state, topicData: action.payload 
-    };
-    case ACTIONS.FETCH_TOPIC_PHOTOS:
-      return { ...state, currentTopic: action.payload 
-    };
-    case ACTIONS.RESET_TOPIC_PHOTOS:
-      return { ...state, currentTopic: null 
-    };
-    case ACTIONS.TOGGLE_MODAL:
-      return {
-        ...state,
-        isModalOpen: !state.isModalOpen,
-        selectedPhotoData: action.payload,
-      };
-    case ACTIONS.CLOSE_MODAL:
-      return {
-        ...state,
-        isModalOpen: false,
-        selectedPhotoData: null,
-      };
-    case ACTIONS.TOGGLE_FAVORITE:
-      const photoId = action.payload;
-      const favoritedPhotos = state.favoritedPhotos.includes(photoId)
-        ? state.favoritedPhotos.filter(id => id !== photoId)
-        : [...state.favoritedPhotos, photoId];
-      return {
-        ...state,
-        favoritedPhotos,
-      };
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
-
-// Custom hook to manage application data
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState); 
 
